6f1f39f36d205b438ccb4cae80df765a9b5556f4
mtgsjr <mtgsjr@hotmail.com>
2024-03-25 12:57:20 -0300

    versao completa

---
+++ views/adicionar.php
@@ -0,0 +1,7 @@
+<center><h3>Inserir Cliente na fila</h3></center>
+<br></br>
+<form method="POST" action="insert.php">
+    <label><h5>Inserir Cliente</h5></label>
+    <input class="form-control" type="text" name="nome" placeholder="Nome">
+    <input type="submit" class="btn btn-success" value="Adicionar na fila">
+</form>
